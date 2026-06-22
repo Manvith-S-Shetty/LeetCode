1a5e46e5a19825f14a4a2cbc3ade4695e517ab96
Manvith-S-Shetty <manumanvith06@gmail.com>
2026-06-22 22:40:24 +0530

    Time: 3 ms (61.87%), Space: 19.4 MB (38.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -7,3 +7,10 @@ class Solution:
                     c['l']//2,
                     c['o']//2,
                     c['n'])
+    '''#remeber the tric is simple BALLO0N B-1 A-1 L-2 O-2 N-1
+    if you got questions like 
+    How many times can I build a word?
+    How many groups can I form?
+    How many copies can I make?
+    use same tric and COUNT THE GIVEN AND TAKE MINIMUM '''
+    

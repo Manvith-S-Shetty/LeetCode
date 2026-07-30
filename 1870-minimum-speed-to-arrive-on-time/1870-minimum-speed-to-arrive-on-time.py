4864fdb95648ceee79a6d50f6e528f367fb2072b
Manvith-S-Shetty <manumanvith06@gmail.com>
2026-07-30 20:42:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        if hour<len(dist)-1:
+            return -1
+        left=1
+        right=10**7
+        while left<right:
+            mid=left+(right-left)//2
+            time=0
+            #mintime=float('inf')
+            for i in range(len(dist)):
+                current=dist[i]/mid
+                if i!=len(dist)-1 and not current.is_integer():
+                        current=math.ceil(current)
+                time+=current
+                
+            
+            if time<=hour:
+                right=mid
+                
+            else:
+                left=mid+1
+        return right
+                
